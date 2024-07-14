5646c308a58e554627451a2687b65c98009db125
Ramune6110 <arduino6110@gmail.com>
2024-07-14 23:06:23 +0900

    [delete]build

--- build/CMakeFiles/arcanain_unittest_template_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/arcanain_unittest_template_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/arcanain_unittest_template_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
